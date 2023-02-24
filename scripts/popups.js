eeaddfa8f3e801242d502b20970b80adfcaf7097
Leandro-icazatti <leaicazatti@gmail.com>
2023-02-24 20:17:24 -0300

    mi primer commit

---
+++ scripts/popups.js
@@ -0,0 +1,28 @@
+let popup = document.getElementById("popup-contacto");
+
+function abrirCont(){
+    popup.classList.add("open-popup-contacto");
+}
+function cerrarCont(){
+    popup.classList.remove("open-popup-contacto");
+}
+
+
+let popup2 = document.getElementById("pop-ciber");
+
+function abrirCiber(){
+    popup2.classList.add("open-pop-ciber");
+}
+function cerrarCiber(){
+    popup2.classList.remove("open-pop-ciber");
+}
+
+
+let popup3 = document.getElementById("popup-info");
+
+function abrirIn(){
+    popup3.classList.add("open-popup-info");
+}
+function cerrarIn(){
+    popup3.classList.remove("open-popup-info");
+}
